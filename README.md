979bccdc1bddd163e326333bb8c2fc68794213c1
DylanWarrener <dqyqlqaqn@gmail.com>
2023-02-28 09:22:55 +0000

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# TPW Electrial Website
+TPW is an electrial firm that are now looking to expand into home development projects.
--- src/stores/stores-about.ts
+++ src/stores/stores-about.ts
@@ -1,23 +1,25 @@
-import { defineStore } from "pinia"
+import { defineStore } from "pinia";
 
-// About interface 
-import IAboutBaseData, { IAboutTextualData } from '@/interfaces/interface-about'
+// About interface
+import IAboutBaseData, { IAboutTextualData } from "@/interfaces/interface-about";
 
-export const useAboutStore = defineStore('aboutStore', {
-    state: (): IAboutBaseData => ({
-        textualData: {
-            canvas: {
-                quoteBox: {
-                    heading: '',
-                    quote: ''
-                }
-            }
-        }
-    }),
-    getters: {
-        getTextualData: (state): IAboutTextualData => state.textualData
-    },
-    actions: {
-        setTextualData(data: any) { this.textualData = data }
-    }
+export const useAboutStore = defineStore("aboutStore", {
+	state: (): IAboutBaseData => ({
+		textualData: {
+			canvas: {
+				quoteBox: {
+					heading: "",
+					quote: "",
+				},
+			},
+		},
+	}),
+	getters: {
+		getTextualData: (state): IAboutTextualData => state.textualData,
+	},
+	actions: {
+		setTextualData(data: any) {
+			this.textualData = data;
+		},
+	},
 });
